eaecb1d31228c0271e4302faf86bb8060652de96
AoifeEdX <ayohwife@gmail.com>
2024-01-16 18:46:17 +0000

    Updated styling and layout

--- script.js
+++ script.js
@@ -33,31 +33,34 @@ const getForecast = async (city) => {
 const displayCurrentWeather = (weatherData) => {
   const { name, dt, main, weather, wind } = weatherData;
 
-  const date = dayjs.unix(dt).format('YYYY-MM-DD HH:mm:ss');
+  const date = dayjs.unix(dt).format('(DD-MM-YYYY)');
   const iconUrl = `https://openweathermap.org/img/w/${weather[0].icon}.png`;
 
   const html = `
-    <h2>${name} - ${date}</h2>
-    <img src="${iconUrl}" alt="${weather[0].description}">
-    <p>Temperature: ${main.temp} °C</p>
-    <p>Humidity: ${main.humidity}%</p>
-    <p>Wind Speed: ${wind.speed} m/s</p>
+    <div id="current-weather" class="mt-5 p-3 border">
+      <h2>${name} ${date}</h2>
+      <img src="${iconUrl}" alt="${weather[0].description}">
+      <p>Temperature: ${main.temp} °C</p>
+      <p>Humidity: ${main.humidity}%</p>
+      <p>Wind Speed: ${wind.speed} m/s</p>
+    </div>
   `;
 
   todaySection.innerHTML = html;
 };
+
 
 const displayForecast = (forecastData) => {
   const forecastList = forecastData.list;
 
   const html = forecastList.slice(0, 5).map((forecast) => {
     const { dt, main, weather } = forecast;
-    const date = dayjs.unix(dt).format('YYYY-MM-DD');
+    const date = dayjs.unix(dt).format('(DD-MM-YYYY)');
     const iconUrl = `https://openweathermap.org/img/w/${weather[0].icon}.png`;
 
     return `
-      <div class="col-md-2 mb-3">
-        <div class="forecast-box">
+      <div class="col-md-2">
+        <div id="forecast-box" class="border p-3 mb-2">
           <h5>${date}</h5>
           <img src="${iconUrl}" alt="${weather[0].description}">
           <p>Temperature: ${main.temp} °C</p>
@@ -103,7 +106,16 @@ document.querySelectorAll('.optional-city').forEach((button) => {
 });
 
 const addToSearchHistory = (city) => {
-  const listItem = document.createElement('li');
+  const listItem = document.createElement('button');
   listItem.textContent = city;
+  listItem.classList.add('btn', 'btn-secondary', 'mb-2');
+  listItem.style.width = '300px'; 
+  listItem.addEventListener('click', async () => {
+    const currentWeather = await getCurrentWeather(city);
+    displayCurrentWeather(currentWeather);
+
+    const forecast = await getForecast(city);
+    displayForecast(forecast);
+  });
   historyList.appendChild(listItem);
 };
